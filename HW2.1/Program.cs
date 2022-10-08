46d97f3a11a3d5511b092b9f2f0675aca48e88f4
Beslan <biozdoev@gmail.com>
2022-10-09 00:48:29 +0300

    First task

---
+++ HW2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿    public enum DayOfWeek
+    {
+        Понедельник = 0,
+        Вторник = 1,
+        Среда = 2,
+        Четверг = 3,
+        Пятница = 4,
+        Суббота = 5,
+        Воскресенье = 6
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("n=");
+            var n = int.Parse(Console.ReadLine());
+             Console.WriteLine((DayOfWeek)((n - 1) % Enum.GetValues(typeof(DayOfWeek)).Length));
+
+             if (n > 5)
+{
+    Console.Write("выходной");
+}
+else
+{
+    Console.Write("будний");
+}
+        }
+    }
