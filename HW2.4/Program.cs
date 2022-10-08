032204407f6dac8708f281d0ac29f5372e1a03a5
Beslan <biozdoev@gmail.com>
2022-10-09 01:37:47 +0300

    Fourth task

---
+++ HW2.4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число ");
+int numberA = int.Parse(Console.ReadLine());
+
+int numberA_5 = numberA % 10;
+int numberA_4 = numberA/10 % 10;
+int numberA_2 = numberA/1000 % 10;
+int numberA_1 = numberA/10000 % 10;
+
+if ((numberA_2==numberA_4) && (numberA_1==numberA_5))
+{
+Console.WriteLine("Число палиндром");
+}
+else
+{
+Console.WriteLine("Число не палиндром");
+}
